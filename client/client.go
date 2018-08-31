97be8637a6e1b92f97a6dcee8d0582e4d1068844
Tabbakha, Ahmad <Ahmad.tabbakha@toyotaconnected.com>
2018-08-31 10:32:31 -0500

    update gitignore, add example client and server code

--- client/client.go
+++ client/client.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"time"
+
+	pb "github.com/whatsadebugger/grpc-examples/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:8080"
 )
 
 func main() {
-	fmt.Println("starting client")
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	no(err)
+	defer conn.Close()
+
+	c := pb.NewPhoneTrackerClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	phone := &pb.PhoneReq{
+		Imei:       "666585287256616",
+		Modelname:  "Samsung Galaxy Note 4",
+		Unlockcode: "44444444",
+	}
+	resp, err := c.AddPhone(ctx, phone)
+	log.Printf("Imei: %+v ModelName: %+v UnlockCode: %+v\n", resp.Imei, resp.Modelname, resp.Unlockcode)
+}
+
+func no(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/whatsadebugger/grpc-examples/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
+type server struct{}
+
+func (s *server) AddPhone(ctx context.Context, in *pb.PhoneReq) (*pb.PhoneResp, error) {
+	return &pb.PhoneResp{
+		Imei:       in.Imei,
+		Modelname:  in.Modelname,
+		Unlockcode: in.Unlockcode,
+	}, nil
+}
+
 func main() {
-	fmt.Println("server started")
+	l, err := net.Listen("tcp", ":8080")
+	no(err)
+	s := grpc.NewServer()
+	pb.RegisterPhoneTrackerServer(s, &server{})
+	no(s.Serve(l))
+}
+
+func no(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
